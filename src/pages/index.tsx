5e42e6d5b600cf70798aa4fcbbe21efeaf862f10
syafar44 <target44135@gmail.com>
2025-10-23 13:35:35 +0800

    Tambahkan Href Ke halaman pesanan baru sementara

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@heroui/react"
+
+const HomePage = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <Button> 
+                ke Halaman Utama
+            </Button>
+        </div>
+    )
+}
+
+export default HomePage
